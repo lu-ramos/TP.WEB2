3e7482d2fc8fa32bc41267a8bc2e44281ad3bbbb
lu-ramos <lramosfranzese@alumnos.exa.unicen.edu.ar>
2023-10-06 17:23:43 -0300

    subo los pocos cambios para mi compañera

--- router.php
+++ router.php
@@ -1,24 +1,48 @@
 <?php
 
-require_once "index.php";
-require_once "items.php";
-require_once "detalles.php";
-require_once "categorias.php";
-require_once "items_categoria.php";
+require_once './app/controllers/juego.controller.php';
+require_once './app/controllers/categoria.controller.php';
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
-$action = 'home'; 
+$action = 'index'; 
 if (!empty( $_GET['action'])) {
     $action = $_GET['action'];
 }
 
+// listarJuegos        ->   juegosController->showJuegos();
+// listarCategorias    ->   categoriaController->showCategoria();
+
+//login     ->  authContoller->showLogin();
+// logout   -> authContoller->logout();
+// auth     ->  authContoller->auth();
+
 $params = explode('/', $action);
 
 switch ($params[0]) { 
-    
+    case 'listarJuegos':
+        $controller = new JuegosController();
+        $controller->showJuegos();
+    break;
+    case 'listarCategorias':
+        $controller = new categoriaController();
+        $controller->showCategoria();
+    break;
 
+
+
+    case 'login':
+        $controller = new AuthController();
+        $controller->showLogin(); 
+        break;
+    case 'auth':
+        $controller = new AuthController();
+        $controller->auth();
+        break;
+    case 'logout':
+        $controller = new AuthController();
+        $controller->logout();
+        break;
 }
 
 
-?>
